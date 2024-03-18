41675b5ed6c4445d761be17086b502faf8e88f01
khushboo24kumari <khushboo.kumari@navgurukul.org>
2024-03-18 23:45:28 +0530

    calculate app

---
+++ script.js
@@ -0,0 +1,12 @@
+const button=document.querySelector(".button")
+const screen=document.querySelector(".screen")
+const firstinput=document.querySelector("#firstinput")
+const secondInput=document.querySelector("#secondinput")
+
+function AddInput(a,b){
+    if (typeof a=="number" && typeof b=="number"){
+        screen.innerHTML=a+b
+    }else screen.innerHTML=parseInt(a)+parseInt(b)
+
+}
+button.addEventListener("click",()=>AddInput(parseInt(firstinput.value),parseInt(secondInput.value)))
